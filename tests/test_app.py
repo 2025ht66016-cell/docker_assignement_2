845dcf615c97d16fd6b454b84130fa7292ebc2c8
sathiyapriya s <2025ht66016@wilp.bits-pilani.ac.in>
2026-05-04 21:11:00 +0530

    Save and commit test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
 import pytest
+import sys, os
+
+# Ensure Python can find ACEest_Fitness.py in parent folder
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from ACEest_Fitness import app
 
 @pytest.fixture
@@ -14,4 +19,23 @@ def test_homepage(client):
 def test_login_page(client):
     response = client.get('/login')
     assert response.status_code == 200
-    assert b"Login" in response.data
+    assert b"Login Page" in response.data
+
+def test_members_page(client):
+    response = client.get('/members')
+    assert response.status_code == 200
+    assert b"Members List" in response.data
+
+def test_classes_page(client):
+    response = client.get('/classes')
+    assert response.status_code == 200
+    assert b"Classes Schedule" in response.data
+
+def test_trainers_page(client):
+    response = client.get('/trainers')
+    assert response.status_code == 200
+    assert b"Trainer Profiles" in response.data
+
+def test_invalid_route(client):
+    response = client.get('/nonexistent')
+    assert response.status_code == 404
